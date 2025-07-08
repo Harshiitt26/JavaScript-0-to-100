85ce4205a193e5fc6429b48808fc002d0a3ee1c7
harshit Kumar <138262761+Harshiitt26@users.noreply.github.com>
2025-07-08 14:02:18 +0000

    variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,13 @@
+const id = 10
+// const keyword can not be changed
+
+let name = "harshit" //  Block Scope
+var place = "India"  //  Global Scope
+//Never use "var" variable
+
+username = "harshiitt26"
+number = 2
+
+let address;
+console.table([id, name, place, username , number , address])
+console.log(username, number , address)
