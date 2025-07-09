b0ed8cbb6855413e6e7dfacad51f600fff5f4606
harshit Kumar <138262761+Harshiitt26@users.noreply.github.com>
2025-07-09 14:20:14 +0000

    Types of dataTypes

--- 01-basics/02-dataTypes.js
+++ 01-basics/02-dataTypes.js
@@ -13,10 +13,24 @@ const isMale = true // Boolean true/false
 let salary = null // null means an empty value.
 let address; // now address is undefined which means the value to address is not defined yet.
 const id = Symbol('id'); // Symbol is used for unique, hidden keys in objects.
-const obj = {
+const bigNumber = 987654321n
+console.log([typeof(5), typeof("Harshit"), typeof(undefined), typeof(null)])
+console.log(typeof null) // This is also called draweback in JS language.
+
+const myFruits = ["Apple", "mango", "banana"] // fruits is an array
+const myObj = {  
     "name": "harshit",
-    "class": "12"
+    "class": 12
+} // obj is an object.
+let myFunction = function(){
+    console.log("Hi")
 }
-// obj is an object.
-console.table([typeof(5), typeof("Harshit"), typeof(undefined), typeof(null)])
-console.log(null) // This is also called draweback in JS language.
+myFunction()
+console.log([myFruits[2] , myFruits[3] , myObj.class])
+console.log([typeof myFruits , typeof myObj , typeof myFunction])
+
+// JavaScript is DYNAMICALLY TYPED coz let x = 5 , engine knows 5 is an number
+// 2 Types of Data Types
+// PRIMITIVE DATA TYPES - 7 - Number , String , Null , Undefined , Boolean , Symbol , BigInt
+
+// REFERENCE (Non-Primitive) - 3 - Array , Objects , Functions
