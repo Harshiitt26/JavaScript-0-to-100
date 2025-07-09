4aad57982699855224e906e2089f0498a7f7ff7d
harshit Kumar <138262761+Harshiitt26@users.noreply.github.com>
2025-07-09 13:50:54 +0000

    Data Types Conversion

---
+++ 01-basics/04-dataTypeComparison.js
@@ -0,0 +1,10 @@
+console.log(["5">2 , "05">2])
+console.log([
+    null > 0,
+    null == 0,
+    null >= 0
+])
+// comparisons convert null to numbers treating it as 0 therefore only null >= 0 is true.
+console.log([undefined > 0 , undefined == 0 , undefined >= 0])
+
+console.log([ "2"==2 , "2"===2])
