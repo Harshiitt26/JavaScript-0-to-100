e764eb1ec40a68f98f94e950a4c2fdd588f7328f
harshit Kumar <138262761+Harshiitt26@users.noreply.github.com>
2025-07-10 17:15:28 +0000

    numbers&math part1

---
+++ 01-basics/06-numbers&maths.js
@@ -0,0 +1,14 @@
+const score = 10.6589
+
+console.log([
+    score,
+    score.toString().length,
+    score.toFixed(2),
+    score.toPrecision(2),
+    score.toPrecision(3)
+])
+let hundreds = 10000000000
+console.log([hundreds , hundreds.toLocaleString("en-IN")])
+
+// *********************** MATHS *********************
+console.log( Math)
