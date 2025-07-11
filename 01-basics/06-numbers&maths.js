06f5e90c7092efa31d3cf1a106c76b5d7fd91b26
harshit Kumar <138262761+Harshiitt26@users.noreply.github.com>
2025-07-11 06:55:20 +0000

    numbers&maths part 2

--- 01-basics/06-numbers&maths.js
+++ 01-basics/06-numbers&maths.js
@@ -12,3 +12,21 @@ console.log([hundreds , hundreds.toLocaleString("en-IN")])
 
 // *********************** MATHS *********************
 console.log( Math)
+console.log([
+    Math.abs(-5),
+    Math.min(2,3,4,5),
+    Math.max(2,3,4,5),
+    Math.ceil(4.5),
+    Math.floor(4.5),
+    Math.round(4.8)
+])
+console.log([
+    Math.random(),
+    Math.random()*10,
+    Math.floor((Math.random()*10) + 1) // +1 to avoid 0.
+])
+
+// generate a value btw 15 to 20
+let min =15 , max = 20
+let value = Math.floor((Math.random()*(max-min+1)) + min)
+console.log(value)
